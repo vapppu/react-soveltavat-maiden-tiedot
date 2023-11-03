f74d26282838ab2f5075cc41edf428b577044164
Veera Hiltunen <veera.hiltunen@gmail.com>
2023-11-03 10:47:28 +0200

    Get country capital, area, flag and languages and display them in countrydata component

--- src/components/CountryData.jsx
+++ src/components/CountryData.jsx
@@ -1,12 +1,21 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-
 const CountryData = ({ country }) => {
 
   if (country) {
     return (
         <section className="country">
             <h2>{country.name.common}</h2>
+            <ul>
+                <li>capital: {country.capital[0]}</li>
+                <li>area: {country.area}</li>
+                <li><figure>
+                    <img src={country.flags.png} alt={country.flags.alt}></img>
+                </figure>
+                </li>
+                <li><h3>languages:</h3>
+                <ul>
+                    {Object.values(country.languages).map((language) => <li>{language}</li>)}
+                </ul></li>
+            </ul>
         </section>
         );
   }
